fb14fe1be4d670612e6bd5aca1406593ea3e3abe
Declan McMahon <declan.mcmahon@student.uq.edu.au>
2023-12-07 12:01:51 +0700

    Changed to gotecqfe-ts in react router

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <Routes>
             <Route path="/" element={<Navigate to="/judge-manager/auth"/>}/>
-            <Route path="/gotecqfe" element={<Navigate to="/judge-manager/auth"/>}/>
+            <Route path="/gotecqfe-ts" element={<Navigate to="/judge-manager/auth"/>}/>
             <Route path="/judge-manager/app" element={<JudgeApp/>}/>
             <Route path="/judge-manager/auth" element={<JudgeAuth/>}/>
             <Route path="/judge-manager/app/user" element={<List type='user' />}/>
